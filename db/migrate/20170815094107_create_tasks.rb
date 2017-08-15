09234fd858fb841f149259ce0647b125248f827a
Christine de la Fuente <c.delafuente88@gmail.com>
2017-08-15 11:44:22 +0200

    added due date to Task

--- db/migrate/20170815094107_create_tasks.rb
+++ db/migrate/20170815094107_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :task
+      t.string :description
 
       t.timestamps
     end
